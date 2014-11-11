290a7b55d8ad016a61e20edd7d4121484ef27b0f
陈曦 <clarkchen@foxmail.com>
2014-11-11 21:15:53 +0800

    Folyd 注释，重新理解动态规划，转移方程

--- Graph/Floyd/FolydShortPath.java
+++ Graph/Floyd/FolydShortPath.java
@@ -3,14 +3,35 @@ package Floyd;
 import Dijskra.DijskraShortPath;
 import basic.Graph;
 
+/**
+ * @author chenxi
+ * Folyd 算法实际上是一个全局求解路径的方法，和Dijsktra不同， Folyd直接是计算全部点的最短路径
+ * 其时间复杂度是 N^3 ，而且只有当整个算法遍历结束之后才能，得出最终的最短路径
+ * 算法步骤简单粗暴
+ * DIS[N][N]
+ * 
+ * for k in [1...N]
+ * 	  for i in [1..N]
+ *       for j in [1..N]
+ *       	if( dis[i][j]<dis[i][k]+dis[k][i])
+ *          	 dis[i][j] =  dis[i][k]+dis[k][i];
+ *          
+ *      
+ * 就是这么暴力，但是可能书上没有讲太明白，这个动态规划，本质上实际上是一个三维的状态转移方程
+ * http://zh.wikipedia.org/zh-cn/弗洛伊德算法   
+ * D_{i,j,k}  = D_{i,k,k-1} + D{k,j,k-1}
+ * 
+ * Path[i][j] 表示 i 到 j ，j 前一位是多少，所以在原图上，如果相邻就设置为i，否则就是-1
+ * 
+ */
 public class FolydShortPath {
 	int [][]Dis;
-	int [][]Pre;
+	int [][]Path;
 	static int max = 1000;
 	public void runFolydShortPath(Graph g)
 	{
 		Dis =  g.AdjacentMatrix;
-		Pre =  new int[g.count][g.count];
+		Path =  new int[g.count][g.count];
 		
 		int i,j,k;
 		for(i=0;i<g.count;i++)
@@ -21,9 +42,11 @@ public class FolydShortPath {
 				if(Dis[i][j]==0) 
 				{
 					Dis[i][j] = FolydShortPath.max;
-					 
+					Path[i][j] = -1;
 				}
-				Pre[j][i] = i;
+				else{
+					Path[i][j] = i;
+				}
 			}
 		}
 		
@@ -38,7 +61,7 @@ public class FolydShortPath {
 					 if(Dis[i][j] >  Dis[i][k]+Dis[k][j])
 					{
 						Dis[i][j] = Dis[i][k]+Dis[k][j];
-						Pre[i][j] = Pre[i][k];
+						Path[i][j] = Path[k][j];
 					}
 				}
 				
@@ -52,27 +75,19 @@ public class FolydShortPath {
  		for(int i = 0;i<g.count;i++)
 		{
  			 if(i==source) continue;
-			 if(Pre[i][source] == source ) rt.addEdge( source,i, Dis[source][i]); 
+			 if(Path[source][i] == source ) rt.addEdge( source,i, Dis[source][i]); 
 			 else
 			 {
 				 int  j = i;
-				 while(Pre[j][source] != source)
+				 while(Path[source][j] != source)
 				 {
-					 rt.addEdge(j, Pre[j][source],Dis[j][Pre[j][source]]);
-					 j = Pre[j][source];
+					 rt.addEdge(j, Path[source][j],Dis[j][Path[source][j]]);
+					 j = Path[source][j];
 				 }
 			 }
 		 
 		}
 		return rt;
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Graph g = DijskraShortPath.init();
-		FolydShortPath fsp = new FolydShortPath();
-		fsp.runFolydShortPath(g);
-		Graph rt = fsp.getNewGraph(0, g);
-		rt.printEdges();
 	}
 
 }
