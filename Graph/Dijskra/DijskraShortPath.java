27090c2aeb76009442148f8d79b1348c43d73654
陈曦 <clarkchen@foxmail.com>
2014-11-11 21:12:12 +0800

    Dijskra 注释添加，自动测试代码添加

--- Graph/Dijskra/DijskraShortPath.java
+++ Graph/Dijskra/DijskraShortPath.java
@@ -2,39 +2,28 @@ package Dijskra;
 
 import basic.Graph;
 
+/**
+ * @author chenxi
+ * Dijskra 是单源最短路劲算法，也就是指定起点，然后求其他各个点到他的距离
+ * 这部分代码，求解完最短距离之后，放到了 dis 数组中，并且产生 以最短距离为边的 一个新图
+ * 
+ * 需要的额外数据，Dis，Visited，Pre（用于建立图，方便回溯输出路径）
+ * 算法流程：
+ * 1. k点开始，设置 Visited，（点k表示最新更新的点）
+ * 2. 更新k点周围的所有的没有被访问过的点（访问过的结点肯定具有更短的长度）
+ * 3. 选取最小的点min，k,min 加入新图，min作为新的k点 回到步骤1
+ * 
+ */
 public class DijskraShortPath {
-	public static Graph init(){
-		int v= 9;
-		Graph g  = new Graph(v);
-		
-		g.addEdge(0, 1, 1);
-		g.addEdge(0, 2, 5);
-		g.addEdge(1, 2, 3); 
-		g.addEdge(1, 3, 7);
-		g.addEdge(1, 4, 5);
-		g.addEdge(2, 4, 1);
-		g.addEdge(2, 5, 7);
-		g.addEdge(3, 6, 3);
-		g.addEdge(3, 4, 2);
-		g.addEdge(4, 5, 3);
-		g.addEdge(4, 6, 6);
-		g.addEdge(4, 7, 9);
-		g.addEdge(5, 5, 5);
-		g.addEdge(6, 7, 2);
-		g.addEdge(6, 8, 7);
-		g.addEdge(7, 8, 4);
-		return g;
-	}
-	
-	public Graph shortestPath(int source,Graph g)
+	int []dis;
+ 	public Graph shortestPath(int source,Graph g)
 	{
 		Graph rt = new Graph(g.count);
 		int [] dis = new int [g.count] ;
 		
 		int [] visited = new int[g.count];
 		int [] pre  = new int[g.count];
-		int i,j;
-		
+		int i,j;	
 		for(i = 0;i<dis.length;i++)
 		{
 			visited [i] = 0;
@@ -42,10 +31,12 @@ public class DijskraShortPath {
 			pre [i] =-1;
 		}
 		dis[source] =  0;
-		visited[source] = 1;
+		
 		int k=source;
 		for(i=0;i<g.count;i++)
 		{
+			visited[k] = 1;
+			System.out.println(k);
 			
 			for(j=0;j<dis.length;j++)
 			{
@@ -55,32 +46,28 @@ public class DijskraShortPath {
 					dis[j] = dis[k]+g.AdjacentMatrix[k][j];
 					pre [j] = k;
 				}
+				
 			}
-			
+			//选最小
 			int min =Integer.MAX_VALUE;
-			for(j = 0;j<dis.length;j++)
-			{
-				if(visited[j] == 0 && min > dis[j])
+			for(j=0;j<dis.length;j++){
+				if(visited[j] == 1) continue;
+				if(min > dis[j])
 				{
 					min = dis[j];
 					k = j;
 				}
 			}
+			
 			if(min<Integer.MAX_VALUE){
-				visited[k] = 1;
-				rt.addEdge(k,pre[k] , g.AdjacentMatrix[pre[k]][k]);
+			 	rt.addEdge(k,pre[k] , g.AdjacentMatrix[pre[k]][k]);
 			}
 		}
+		this.dis = dis;
 		return rt;
 	}
 	
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		DijskraShortPath dsp = new DijskraShortPath();
-		Graph g = dsp.init();
-		Graph rt =  dsp.shortestPath(0, g);
-		rt.printEdges();
-	}
+ 
 
 }
